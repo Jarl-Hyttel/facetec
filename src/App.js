e75df0299c1373fab623ed65d1cde9657a0c002f
Jarl Hyttel <jarl.hyttel@gmail.com>
2019-08-26 18:15:15 +0200

    added s to http

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch("http://thawing-reaches-89716.herokuapp.com/imageurl", {
+      fetch("https://thawing-reaches-89716.herokuapp.com/imageurl", {
             method: "post",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("http://thawing-reaches-89716.herokuapp.com/image", {
+          fetch("https://thawing-reaches-89716.herokuapp.com/image", {
             method: "put",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
